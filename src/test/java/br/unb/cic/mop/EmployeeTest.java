97daaab8334142fe209180315a93f402ea18ee8c
Rodrigo Bonifácio <rbonifacio123@gmail.com>
2022-07-12 10:32:55 -0300

    exploring a more expressive ERE formular

--- src/test/java/br/unb/cic/mop/EmployeeTest.java
+++ src/test/java/br/unb/cic/mop/EmployeeTest.java
@@ -18,6 +18,35 @@ public class EmployeeTest {
         assertEquals(100.0, emp.totalSalary(), 0.005);
     }
 
+    @Test
+    public void testValidSequence03() {
+        Employee emp = new Employee("rbonifacio", 100);
+        emp.setSalary(1000);
+        assertEquals(1000.0, emp.totalSalary(), 0.005);
+    }
+
+    @Test
+    public void testValidSequence04() {
+        Employee emp = new Employee("rbonifacio");
+        emp.setSalary(1000);
+        emp.setSalary(5000);
+        assertEquals(5000.0, emp.totalSalary(), 0.005);
+    }
+
+    @Test
+    public void testValidSequence05() {
+        Employee emp = new Employee("rbonifacio");
+
+        emp.setSalary(5000);
+
+        double res = emp.totalSalary();
+        assertEquals(5000.0, res, 0.005);
+
+        emp.setSalary(1000);
+        res = emp.totalSalary();
+        assertEquals(1000.0, res, 0.005);
+    }
+
     @Test(expected = RuntimeException.class)
     public void testInvalidSequence01() {
         Employee emp = new Employee("rbonifacio");
